7fa9fd3dbae0caa322e12d37dc3c4273e728c30d
eabderh <eabderh@clemson.edu>
2016-04-14 07:05:32 +0000

    updated setup scripts
    testing: unsuccesful.
    problems with installing cmake.

--- setup/ccnx-setup.sh
+++ setup/ccnx-setup.sh
@@ -2,4 +2,10 @@
 
 
 git clone https://github.com/PARC/CCNx_Distillery
+sudo apt-get install cmake
+# errors with this command
+cd CCNx_Distillery
+make update
+make all
+
 
--- setup/geni-setup.py
+++ setup/geni-setup.py
@@ -6,28 +6,32 @@ from subprocess import call
 
 lrange = map(str, range(1,8))
 hosts = list("h" + member for member in map(str, range(0,2)))
-nodes = list("b" + member for member in map(str, range(0,1)))
+nodes = list("n" + member for member in map(str, range(0,1)))
 targets = hosts + nodes
 
 print targets
+class geni_setup:
+    def exec_ssh_commands(self, targets, ssh_commands):
+        for target in targets:
+            for ssh_command in ssh_commands:
+                command = "ssh {} \"{}\"".format(target, ssh_command)
+                print command
+                call(command, shell=True)
 
-def exec_ssh_commands(targets, ssh_commands):
-    for target in targets:
-        for ssh_command in ssh_commands:
-            command = "ssh {} \"{}\"".format(target, ssh_command)
+    def exec_remote_script(self, targets, script):
+        for target in targets:
+            command = "scp {} {}:.".format(script, target)
             print command
-            #call(command, shell=True)
+            call(command, shell=True)
+        ssh_commands = ["sudo chmod +x " + script, "./" + script]
+        self.exec_ssh_commands(targets, ssh_commands)
+    def run(self):
+        exec_remote_script(targets, "ccnx-setup.sh")
 
-def exec_remote_script(targets, script):
-    for target in targets:
-        command = "scp {} {}:.".format(script, target)
-        print command
-        #call(command, shell=True)
-    ssh_commands = ["sudo chmod +x " + script, "./" + script]
-    exec_ssh_commands(targets, ssh_commands)
+testing = geni_setup()
 
-
-exec_remote_script(targets, "test.sh")
-
+#testing.exec_remote_script(targets, "ccnx-setup.sh")
+#testing.exec_ssh_commands(host, ["cd CCNx_Distillery"])
+#testing.exec_ssh_commands(targets, ["make"])
 
 
